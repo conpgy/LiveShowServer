984487fc63e6aaf4cc2468c6d044659a0133fc02
conpgy <conpgy@gmail.com>
2016-12-12 22:43:37 +0800

    Initial Commit

---
+++ LiveShowServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveShowServer
+//
+//  Created by penggenyong on 16/12/12.
+//  Copyright © 2016年 penggenyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
