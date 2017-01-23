87a1236ceda49560e6c81e9fe7f3d29854a6c173
pgy <conpgy@gmail.com>
2017-01-23 10:23:37 +0800

    package update

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     ],
 
 	dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",       majorVersion: 1, minor: 4),
-		.Package(url: "https://github.com/IBM-Swift/Kitura.git",             majorVersion: 1, minor: 4),
-		.Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",       majorVersion: 1, minor: 5),
+		.Package(url: "https://github.com/IBM-Swift/Kitura.git",             majorVersion: 1, minor: 5),
+		.Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL", majorVersion: 0, minor: 5),
 		.Package(url: "https://github.com/davidungar/miniPromiseKit",        majorVersion: 4, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Markdown", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 5)
 	]
 )
